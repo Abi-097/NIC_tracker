411f2832a71aa4e88d9c6925249e2f9b23178c2d
Abi-097 <abimahen0312@gmail.com>
2023-03-19 20:27:47 +0530

    changes done1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import View from './Common/Pages/View';
 
-
+ 
 
 function App() {
    return(
-
     <View/>
    )
 }
--- src/Common/Pages/View.js
+++ src/Common/Pages/View.js
@@ -1,7 +1,7 @@
-import {React, ReactDOM} from "react";
+import {React} from "react";
 import { useState } from "react";
 import {Button, FloatingLabel, Form} from 'react-bootstrap';
-// import lankaNIC from "lanka-nic";
+import lankaNIC from "lanka-nic";
 import './view.css'
 
 
@@ -11,58 +11,46 @@ export default function View(){
     const [nic, setNic] = useState('');
     const [dob, setDob] = useState('');
     const [gender, setGender] = useState('');
-    const [data,setData] = useState([]);
 
-        function calView(){
-            const ob = {
-             NICNo:nic,   
-            DOB: dob,
-            Gender: gender,
-            }
-        const ob1 = [...data];
-    ob1.push(ob);
-    setData(ob1);
-    clear();
+         
+const searching = () => {
+    if(nic.length === 10 || nic.length === 12){
+        let { dateOfBirth, gender } = lankaNIC.getInfoFromNIC(nic);
+        setDob(new Date(dateOfBirth).toISOString().slice(0, 10));
+        setGender(gender);
+        console.log (new Date(dateOfBirth).toISOString().slice(0, 10));
+        console.log(gender);
+    }
+    else(
+        alert("There is an error on your NIC No....")
+    )
+    
 }
-function clear () {
-    setNic("");
-}
-        
     return (
 
         <div className="Viewer">
 
 <div className='w-25 border rounded m-5 p-5 mt-5 bg-light'>
-            {/* const lankaNIC = require("lanka-nic"); */}
-            {/* let { dob, gender } = lankaNIC.getInfoFromNIC("925182566V"); */}
-
-
             <FloatingLabel
-        controlId="floatingInput"
-        label="NIC NO"
-        className="mb-3"
-      >
-        <Form.Control type="email" value = {nic} placeholder=" " onChange={(v)=> setNic(v.target.value)}  />
+                controlId="floatingInput"
+                label="National ID"
+                className="mb-3"
+            >
+        <Form.Control type="text" value = {nic} placeholder=" " onChange={(v)=> setNic(v.target.value)}  />
       </FloatingLabel>
       <br/>
-            <Button variant="outline-primary" onClick={()=>setNic(nic)}>Search</Button>{' '}
+            <Button className="Btn_Search" variant="outline-primary" onClick={(searching)}> Search</Button>
             <br/><br/>
             <div>
-            <label> NIC NO:{nic}</label>
+            <label> National ID:{nic}</label>
             <br/>
             <br/>
-            <label> DOB:{dob}</label>
+            <label> Date Of Birth:{dob}</label>
             <br/>
             <br/>
-            <label>GENDER:{gender}</label>
+            <label> Gender:{gender}</label>
             </div>
             <br/>
-
-            <div>
-                {data.map((val)=> (
-                    <View nic={val.NICNo}/>
-                ))}
-            </div>
         </div>
         </div>
         
