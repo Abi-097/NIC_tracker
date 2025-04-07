1605e069a1a3bc696f2d4bd0d61ca0ccf1ee120a
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2025-04-07 21:17:56 +0530

    error fixed

--- Components/NicGenerator.tsx
+++ Components/NicGenerator.tsx
@@ -33,12 +33,6 @@ const d_array: MonthData[] = [
   { month: "November", days: 30 },
   { month: "December", days: 31 },
 ];
-const getFormattedDate = (date: Date): string => {
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-  return `${day}-${month}-${year}`;
-};
 
 export default function NICValidator() {
   const [nicNumber, setNicNumber] = useState<string>("");
