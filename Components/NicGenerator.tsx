51e91a4be164a99fe3e61ba448bcfcd66731e4f1
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2025-04-07 21:17:01 +0530

    format chnaged

--- Components/NicGenerator.tsx
+++ Components/NicGenerator.tsx
@@ -187,5 +187,5 @@ function getFormattedDate(date: Date): string {
   const year = date.getFullYear();
   const month = (1 + date.getMonth()).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
-  return `${month}/${day}/${year}`;
+  return `${day}-${month}-${year}`;
 }
